594a1facd1199e49d81bbe0bb1a7b4e2ba6994f4
David Fernandes <davidfndssdev@gmail.com>
2025-04-22 19:03:20 -0300

    Feat: Select verses

--- src/app/[bookabbrev]/[chapter]/[verse]/page.tsx
+++ src/app/[bookabbrev]/[chapter]/[verse]/page.tsx
@@ -1,18 +1,24 @@
+"use client"
+
 
 import nvi from "@/app/bible/nvi.json";
+import { useParams } from "next/navigation";
 
 const bible: BibleBook[] = nvi;
 
-export default function Home() {
+export default function ReadPage() {
+ const {bookabbrev, chapter, verse} = useParams()
+
+
   return (
     <main className="flex w-full max-w-[400px] border border-zinc-800 px-4 min-h-screen flex-col items-start mx-auto py-20">
-      <h1 className="font-bold text-4xl">{`${bible[0].name} ${bible[0].chapters.indexOf(bible[0].chapters[0]) + 1}`}</h1>
+      <h1 className="font-bold text-4xl">{`${bible.find(book => bookabbrev == book.abbrev)?.name} ${chapter}`}</h1>
 
 
       {/* Verses division */}
       <div className="flex flex-col text-xl gap-1 mt-4">
         {
-          bible[0].chapters[0].map((verse, index) => (
+          bible.find(book => bookabbrev == book.abbrev)?.chapters[chapter - 1].map((verse, index) => (
             <p key={index}>{`${index + 1}. ${verse}`}</p>
           ))
         }
---
+++ src/app/interfaces/BibleBook.ts
@@ -0,0 +1,7 @@
+interface BibleBook {
+    abbrev: string;
+    chapters: string[][];
+    name: string;
+}
+  
+type Biblia = BibleBook[];
